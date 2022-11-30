4a4a58a35c597d1b067f0927e3c4f8f1dcc469e5
rayrr13 <rayrubiarankin@gmail.com>
2022-11-30 10:51:45 +0000

    adding initial lines of code

--- scripts/tutorial_NMDS.R
+++ scripts/tutorial_NMDS.R
@@ -5,3 +5,24 @@
 ####                  Created on 29/11/2022                 ####
 #                                                              #
 ##%##########################################################%##
+
+# set working directory----
+setwd("your_filepath")
+
+# install packages-----
+install.packages("vegan")
+install.packages("permute")
+install.packages("lattice")
+install.packages("tidyverse")
+
+# load libraries----
+library(vegan) # for diversity analysis and ordination methods
+library(permute) # may be necessary for vegan package
+library(lattice) # may be necessary for vegan package
+library(tidyverse) # for efficient data manipulation and visualization
+
+# import data----
+inverts <- read.csv("data/invert_data.csv")
+
+
+
